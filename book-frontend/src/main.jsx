318665e4508dc1b705ee06d5a92251aa1a9989bb
Kapil <kapil@dtdc.com>
2026-01-24 07:40:04 +0530

    add chakkra ui and redux

--- book-frontend/src/main.jsx
+++ book-frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./store";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>
+);
